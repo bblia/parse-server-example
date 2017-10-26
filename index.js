b5fc770e4e4dee4b6cb01c8b1fd3b1bea98573a4
lzadoyan <lzadoyan@hearsaycorp.com>
2017-10-25 20:13:13 -0700

    adding apple app site association crap

--- index.js
+++ index.js
@@ -41,8 +41,8 @@ app.get('/', function(req, res) {
 
 // There will be a test page available on the /test path of your server url
 // Remove this before launching your app
-app.get('/test', function(req, res) {
-  res.sendFile(path.join(__dirname, '/public/test.html'));
+app.get('/apple-app-site-association', function(req, res) {
+  res.sendFile(path.join(__dirname, '/public/apple-app-site-association'));
 });
 
 app.get('/colors/:color', function(req, res) {
